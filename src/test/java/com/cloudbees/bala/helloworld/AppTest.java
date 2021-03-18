36994d036d770a81c72a193f309feefc19667d21
sekharm189 <57168653+sekharm189@users.noreply.github.com>
2021-03-18 20:46:27 +0530

    Update AppTest.java

--- src/test/java/com/cloudbees/bala/helloworld/AppTest.java
+++ src/test/java/com/cloudbees/bala/helloworld/AppTest.java
@@ -2,7 +2,7 @@ package com.cloudbees.bala.helloworld;
 
 import junit.framework.Test;
 import junit.framework.TestCase;
-//import junit.framework.TestSuite;
+import junit.framework.TestSuite;
 
 
 /**
